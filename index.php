4c3caf232d932daff98ae8953d5cf542044c2b27
AndreVictor60 <andre.victor60@free.fr>
2018-01-03 10:57:45 +0100

    Commentaire

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Partie 7 Exercice 3</title>
-  </head>
-  <body>
-    <form action="user.php" method="get">
-      <label for="firstName">Nom :</label><input type="text" name="firstName">
-      <label for="lastName">Prénom :</label><input type="text" name="lastName">
-      <input type="submit" value="valider">
-    </form>
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <title>Partie 7 Exercice 3</title>
+    </head>
+
+    <body>
+        <p>Avec le formulaire de l'exercice 1, afficher dans la page user.php les données du formulaire transmis.</p>
+        <hr>
+        <!-- Création du formulaire
+     Action = L'attribut sert à définir la page appelée par le formulaire
+     Method(variable globale) = Méthode d'envoie du formulaire GET ou POST(99%)
+        -->
+        <form action="user.php" method="get">
+            <!-- Ajout des inputs avec leurs labels -->
+            <label for="firstName">Nom :</label><input type="text" name="firstName">
+            <label for="lastName">Prénom :</label><input type="text" name="lastName">
+            <!-- Submit : le bouton de validation du formulaire qui commande l'envoi des donnée-->
+            <input type="submit" value="valider">
+        </form>
+        <a href="../index.php"><button type="button">Retour</button></a>
+    </body>
 </html>
--- user.php
+++ user.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Partie 7 Exercice 3</title>
-  </head>
-  <body>
-    <?php
-      if (isset($_GET['firstName']) AND isset($_GET['lastName']))
-      {
-        echo 'Bonjour, ' . $_GET['firstName'] . ' ' . $_GET['lastName'];
-      }
-     ?>
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <title>Partie 7 Exercice 3</title>
+    </head>
+    <body>
+        <?php
+        // Vérification des GET s'ils existent
+        if (isset($_GET['firstName']) AND isset($_GET['lastName'])) {
+            // htmlspecialchars qui sert à échapper le code HTML ( Éviter les failles XSS )
+            echo 'Bonjour, ' . htmlspecialchars($_GET['firstName']) . ' ' . htmlspecialchars($_GET['lastName']);
+        }
+        ?>
+    </body>
 </html>
